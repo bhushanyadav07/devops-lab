5983e394c4068ad586d52ec3d679367765417b71
bhushan <bhushanyadav8939@gmail.com>
2026-03-04 16:14:00 +0530

    updated serber.js response meesage

--- app/server.js
+++ app/server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    res.write("Hello DevOps CI/CD Pipelineee develop by bhushan bhau yadav patil");
+    res.write("Hello DevOps CI/CD Pipelineee develop by DevOps Team");
     res.end();
 });
 
